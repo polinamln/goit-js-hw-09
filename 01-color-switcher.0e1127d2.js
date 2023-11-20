a4006574cbab90931b2a7e94af8ddd0c6d434f37
polinamln <137650718+polinamln@users.noreply.github.com>
2023-11-20 00:28:25 +0000

    Deploying to gh-pages from @ polinamln/goit-js-hw-09@17cd5e596b4f12f23ae28d0933f69e971c32c050 🚀

---
+++ 01-color-switcher.0e1127d2.js
@@ -0,0 +1,2 @@
+!function(){var t,n={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};n.btnStart.addEventListener("click",(function(){n.btnStart.disabled=!0,n.btnStop.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.btnStop.addEventListener("click",(function(){n.btnStart.disabled=!1,n.btnStop.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.0e1127d2.js.map
