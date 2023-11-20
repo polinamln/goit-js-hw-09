83beaee9b4b2ab5152c7425badf59ae94c88167f
polinamln <polina77979.la@gmail.com>
2023-11-20 01:17:00 +0100

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]')
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', stopChangingColor);
+
+let intervalId;
+
+function changeColor() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function stopChangingColor() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+
+    clearInterval(intervalId);
+};
---
+++ src/js/03-promises.js
@@ -0,0 +1,42 @@
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit)
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  })
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  for (let i = 0; i < amount; i++){
+    createPromise(i + 1, delay + i * step)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  }
+}
+
+
+
+
